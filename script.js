db79d827f3e9c03f189ac2abedcae61d8d392c67
bhairusingh77 <bhairusinghchadana@gmail.com>
2024-05-20 16:38:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,4 +58,15 @@ document.addEventListener("DOMContentLoaded", function() {
     autoChangeSlide();
 });
 
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const message = document.getElementById('message').value.trim();
+  
+    if (!name || !email || !message) {
+      alert('Please fill in all fields.');
+      event.preventDefault();
+    }
+  });
+  
 
